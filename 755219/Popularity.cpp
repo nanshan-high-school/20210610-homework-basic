4709b7d255dd8668a00169407c85f40f670ad4d8
bradychen950410 <46493214+bradychen950410@users.noreply.github.com>
2021-06-17 01:28:00 +0800

    Edit Popularity.cpp

--- 755219/Popularity.cpp
+++ 755219/Popularity.cpp
@@ -5,36 +5,45 @@ int main() {
     int width, height;
     cin >> width >> height;
     int arr[width][height];
-    for (int i = 0; i < width; i++) {
-        for (int j = 0; j < height; j++) {
+    for (int j = 1; j < height+1; j++) {
+        for (int i = 1; i < width+1; i++) {
             cin >> arr[i][j];
         }
     }
-    //邊緣歸零（為什麼沒辦法歸零啦！
-    for (int i = 0; i < width; i++) {
-        arr[i][-1] = 0;
-        arr[i][height] = 0;
+    //邊緣歸零(skip)
+    //打分數
+    int amounts = 4, temp = 0;
+    for (int j = 1; j < height+1; j++) {
+        for (int i = 1; i < width+1; i++) {
+            if (arr[i][j] == 0) {
+                if (arr[i][j-1] == 0 || arr[i][j-1] > 1000) {
+                    amounts -= 1;
+                } else {
+                    temp += arr[i][j-1];
+                }
+                if (arr[i][j+1] == 0 || arr[i][j+1] > 1000) {
+                    amounts -= 1;
+                } else {
+                    temp += arr[i][j+1];
+                }
+                if (arr[i-1][j] == 0 || arr[i-1][j] > 1000) {
+                    amounts -= 1;
+                } else {
+                    temp += arr[i-1][j];
+                }
+                if (arr[i+1][j] == 0 || arr[i+1][j] > 1000) {
+                    amounts -= 1;
+                } else {
+                    temp += arr[i+1][j];
+                }
+                temp /= amounts;
+                arr[i][j] = temp;
+            }
+        }
     }
-    for (int j = 0; j < height; j++) {
-        arr[-1][j] = 0;
-        arr[width][j] = 0;
-    }
-
-    // int temp;
-    // for (int i = 0; i < width; i++) {
-    //     for (int j = 0; j < height; j++) {
-    //         if (arr[i][j] == 0) {
-    //             temp = arr[i+1][j] + arr[i-1][j] + arr[i][j+1] + arr[i][j-1];
-    //             temp /= 4;
-    //             arr[i][j] = temp;
-    //         }
-    //     }
-    // }
-
     //Output
-    cout << "-----" << endl;
-    for (int i = 0; i < width; i++) {
-        for (int j = 0; j < height; j++) {
+    for (int j = 1; j < height+1; j++) {
+        for (int i = 1; i < width+1; i++) {
             cout << arr[i][j] << " ";
         }
         cout << endl;
